d88f5f2aa3c3d2c04292076f8e48d37ecc582f46
Josef Caique <jcaique10@gmail.com>
2025-07-16 16:32:03 -0300

    creating ClientDTO

---
+++ src/main/java/com/josef/digitalBank/bankapi/dto/ClientDTO.java
@@ -0,0 +1,81 @@
+package com.josef.digitalBank.bankapi.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class ClientDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String cpf;
+    private String email;
+
+    @JsonFormat(pattern = "dd/mm/yyyy")
+    private LocalDate birthdate;
+
+    public ClientDTO(){}
+
+    public ClientDTO(Long id, String name, String lastName, String cpf, String email, LocalDate birthdate) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.cpf = cpf;
+        this.email = email;
+        this.birthdate = birthdate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+}
