5e0e6514aa737d86127177f2fccdcc63ef265a07
Josef Caique <jcaique10@gmail.com>
2025-07-16 16:40:49 -0300

    creating mapper with dozer

---
+++ src/main/java/com/josef/digitalBank/bankapi/mapper/ObjectMapper.java
@@ -0,0 +1,24 @@
+package com.josef.digitalBank.bankapi.mapper;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectMapper {
+
+    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+    public static <O, D> D parseObject(O origin, Class<D> destination) {
+        return mapper.map(origin, destination);
+    }
+
+    public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
+        List<D> objectsDestination = new ArrayList<D>();
+        for (O object: origin) {
+            objectsDestination.add(mapper.map(object, destination));
+        }
+        return objectsDestination;
+    }
+}
