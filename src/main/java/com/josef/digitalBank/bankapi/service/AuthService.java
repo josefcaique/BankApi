a23aac964c2fd41124c167f17ed41c7ed0ba1e52
Josef Caique <jcaique10@gmail.com>
2025-07-31 00:22:58 -0300

    building Auth service

---
+++ src/main/java/com/josef/digitalBank/bankapi/service/AuthService.java
@@ -0,0 +1,51 @@
+package com.josef.digitalBank.bankapi.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.josef.digitalBank.bankapi.model.Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class AuthService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(Client client) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("BankApi")
+                    .withSubject(client.getEmail())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error while generating token!");
+
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("BankApi")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
