84fa2a2b0473975e456d109660b5ec4bedf5b032
Josef Caique <jcaique10@gmail.com>
2025-08-01 23:22:25 -0300

    creating JwtTokenProvider

---
+++ src/main/java/com/josef/digitalBank/bankapi/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,32 @@
+package com.josef.digitalBank.bankapi.security.jwt;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Service
+public class JwtTokenProvider {
+
+    @Value("${security.jwt.token.secret-key:secret}")
+    private String secretKey;
+
+    @Value("${security.jwt.token.expire-length:3600000}")
+    private Long validityInMilliSeconds;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    Algorithm algorithm = null;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        algorithm = Algorithm.HMAC256(secretKey.getBytes());
+    }
+}
