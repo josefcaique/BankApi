2e2361dda8e99503375c896325a4f8e13c93e6b4
Josef Caique <jcaique10@gmail.com>
2025-08-03 00:32:34 -0300

    implemented securityConfig

--- src/main/java/com/josef/digitalBank/bankapi/BankapiApplication.java
+++ src/main/java/com/josef/digitalBank/bankapi/BankapiApplication.java
@@ -10,4 +10,5 @@ public class BankapiApplication {
 		SpringApplication.run(BankapiApplication.class, args);
 	}
 
+
 }
--- src/main/java/com/josef/digitalBank/bankapi/config/SecurityConfig.java
+++ src/main/java/com/josef/digitalBank/bankapi/config/SecurityConfig.java
@@ -1,13 +1,23 @@
 package com.josef.digitalBank.bankapi.config;
 
+import com.josef.digitalBank.bankapi.security.jwt.JwtTokenFilter;
 import com.josef.digitalBank.bankapi.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,5 +45,33 @@ public class SecurityConfig {
 
         passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Enconder);
         return passwordEncoder;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtTokenFilter filter = new JwtTokenFilter(tokenProvider);
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        authorizeHttpRequests -> authorizeHttpRequests
+                                .requestMatchers(
+                                        "/auth/signin",
+                                        "/auth/refresh",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**"
+
+                                ).permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/client").permitAll()
+                                .requestMatchers("/api/client").denyAll()
+                )
+                .build();
     }
 }
