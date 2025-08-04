b8e99655411c820b7ef2f6be73f345630c6ade9b
Josef Caique <jcaique10@gmail.com>
2025-08-03 23:26:26 -0300

    implementing sign in

---
+++ src/main/java/com/josef/digitalBank/bankapi/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.josef.digitalBank.bankapi.controller;
+
+import com.josef.digitalBank.bankapi.dto.authDTO.AuthDTO;
+import com.josef.digitalBank.bankapi.dto.authDTO.TokenDTO;
+import com.josef.digitalBank.bankapi.service.AuthService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/auth")
+@Controller
+public class AuthController {
+
+    @Autowired
+    AuthService service;
+
+    @GetMapping(value = "/signin")
+    public ResponseEntity<?> signin(@RequestBody AuthDTO authDTO) {
+        if (credentialsNull(authDTO)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+
+        var token = service.signIn(authDTO);
+        if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        return ResponseEntity.ok().body(token);
+    }
+
+
+    private static boolean credentialsNull(AuthDTO authDTO) {
+        return authDTO == null || StringUtils.isBlank(authDTO.password()) || StringUtils.isBlank(authDTO.login());
+    }
+}
