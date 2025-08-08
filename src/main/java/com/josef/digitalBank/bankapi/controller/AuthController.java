f58de68441859f18af58f88c72574563c1399c70
Josef Caique <jcaique10@gmail.com>
2025-08-07 21:59:43 -0300

    adding refresh token in controller

--- src/main/java/com/josef/digitalBank/bankapi/controller/AuthController.java
+++ src/main/java/com/josef/digitalBank/bankapi/controller/AuthController.java
@@ -1,16 +1,13 @@
 package com.josef.digitalBank.bankapi.controller;
 
 import com.josef.digitalBank.bankapi.dto.authDTO.AuthDTO;
-import com.josef.digitalBank.bankapi.dto.authDTO.TokenDTO;
 import com.josef.digitalBank.bankapi.service.AuthService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/auth")
 @Controller
@@ -27,6 +24,17 @@ public class AuthController {
         return ResponseEntity.ok().body(token);
     }
 
+    @PutMapping(value="/refresh/{username}")
+    public ResponseEntity<?> refresh(@PathVariable("username") String username, @RequestHeader("Authorization") String refreshToken) {
+        if (parametersAreInvalid(username, refreshToken)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        var token = service.refreshToken(username, refreshToken);
+        if (token == null) ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        return ResponseEntity.ok().body(token);
+    }
+
+    private boolean parametersAreInvalid(String username, String refreshToken) {
+        return StringUtils.isBlank(username) || StringUtils.isBlank(refreshToken);
+    }
 
     private static boolean credentialsNull(AuthDTO authDTO) {
         return authDTO == null || StringUtils.isBlank(authDTO.password()) || StringUtils.isBlank(authDTO.login());
